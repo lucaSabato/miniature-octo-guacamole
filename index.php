cdfffc188870ed04105b61dedcec1bda4d0dc314
Luca Sabato <61189438+lucaSabato@users.noreply.github.com>
2021-11-13 15:06:04 +0000

    initial design of the App class

--- index.php
+++ index.php
@@ -2,7 +2,11 @@
 
 require_once 'bootstrap.php';
 
+define("CBOOKAPICLIENT", "8XWr1Hp5xbhSM6u0");
+define("CBOOKAPISECRET", "07xdHM9OA8ULaBeC");
+define("CRITTERAPIKEY", "j4nHNuaPo2nq6AdW");
 
-$app = new App();
+
+$app = new App(CBOOKAPICLIENT, CBOOKAPISECRET, CRITTERAPIKEY);
 
 $app->render();
--- src/App.php
+++ src/App.php
@@ -3,6 +3,48 @@
 class App
 {
     /**
+     * Critter API Key used for authentication.
+     *
+     * @var string
+     */
+    private $_apiKey;
+
+    /**
+     * CBook API Client Key used for authentication.
+     *
+     * @var string
+     */
+    private $_apiClientKey;
+
+    /**
+     * CBook Secret Key used for authentication.
+     *
+     * @var string
+     */
+    private $_apiClientSecret;
+
+    /**
+     * CBook Friends
+     *
+     * @var array
+     */
+    public $friends;
+
+    /**
+     * Critter Followers
+     *
+     * @var array
+     */
+    public $followers;
+
+    public function __construct(string $apiClientKey, string $apiClientSecret, string $apiKey)
+    {
+        $this->_apiClientKey = $apiClientKey;
+        $this->_apiClientSecret = $apiClientSecret;
+        $this->_apiKey = $apiKey;
+    }
+
+    /**
      * Render the page.
      *
      * @return void
