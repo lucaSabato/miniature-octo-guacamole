0f05901f39eea182df0577eca9d2b38bbac090b3
Luca Sabato <61189438+lucaSabato@users.noreply.github.com>
2021-11-13 16:35:20 +0000

    project Completed

--- header.php
+++ header.php
@@ -21,69 +21,69 @@
               font-size: 3.5rem;
             }
         }
-
         .b-example-divider {
-            height: 3rem;
-            background-color: rgba(0, 0, 0, .1);
-            border: solid rgba(0, 0, 0, .15);
-            border-width: 1px 0;
-            box-shadow: inset 0 .5em 1.5em rgba(0, 0, 0, .1), inset 0 .125em .5em rgba(0, 0, 0, .15);
+          height: 3rem;
+          background-color: rgba(0, 0, 0, .1);
+          border: solid rgba(0, 0, 0, .15);
+          border-width: 1px 0;
+          box-shadow: inset 0 .5em 1.5em rgba(0, 0, 0, .1), inset 0 .125em .5em rgba(0, 0, 0, .15);
         }
 
         .bi {
-            vertical-align: -.125em;
-            fill: currentColor;
+          vertical-align: -.125em;
+          fill: currentColor;
         }
 
-        .feature-icon {
-            display: inline-flex;
-            align-items: center;
-            justify-content: center;
-            width: 4rem;
-            height: 4rem;
-            margin-bottom: 1rem;
-            font-size: 2rem;
-            color: #fff;
-            border-radius: .75rem;
+        .opacity-50 { opacity: .5; }
+        .opacity-75 { opacity: .75; }
+
+        .list-group {
+          width: auto;
+          max-width: 460px;
+          margin: 4rem auto;
         }
 
-        .icon-link {
-            display: inline-flex;
-            align-items: center;
-        }
-        .icon-link > .bi {
-            margin-top: .125rem;
-            margin-left: .125rem;
-            transition: transform .25s ease-in-out;
-            fill: currentColor;
-        }
-        .icon-link:hover > .bi {
-            transform: translate(.25rem);
+        .form-check-input:checked + .form-checked-content {
+          opacity: .5;
         }
 
-        .icon-square {
-            display: inline-flex;
-            align-items: center;
-            justify-content: center;
-            width: 3rem;
-            height: 3rem;
-            font-size: 1.5rem;
-            border-radius: .75rem;
+        .form-check-input-placeholder {
+          pointer-events: none;
+          border-style: dashed;
+        }
+        [contenteditable]:focus {
+          outline: 0;
         }
 
-        .rounded-4 { border-radius: .5rem; }
-        .rounded-5 { border-radius: 1rem; }
-
-        .text-shadow-1 { text-shadow: 0 .125rem .25rem rgba(0, 0, 0, .25); }
-        .text-shadow-2 { text-shadow: 0 .25rem .5rem rgba(0, 0, 0, .25); }
-        .text-shadow-3 { text-shadow: 0 .5rem 1.5rem rgba(0, 0, 0, .25); }
-
-        .card-cover {
-            background-repeat: no-repeat;
-            background-position: center center;
-            background-size: cover;
+        .list-group-checkable {
+          display: grid;
+          gap: .5rem;
+          border: 0;
+        }
+        .list-group-checkable .list-group-item {
+          cursor: pointer;
+          border-radius: .5rem;
+        }
+        .list-group-item-check {
+          position: absolute;
+          clip: rect(0, 0, 0, 0);
+          pointer-events: none;
+        }
+        .list-group-item-check:hover + .list-group-item {
+          background-color: var(--bs-light);
+        }
+        .list-group-item-check:checked + .list-group-item {
+          color: #fff;
+          background-color: var(--bs-blue);
+        }
+        .list-group-item-check[disabled] + .list-group-item,
+        .list-group-item-check:disabled + .list-group-item {
+          pointer-events: none;
+          filter: none;
+          opacity: .5;
         }
     </style>
 
   </head>
   <body>
+<main>
--- src/App.php
+++ src/App.php
@@ -65,6 +65,70 @@ class App
      */
     public function render()
     {
-        echo "<p>it Works!</p>"
+        $this->renderAssiociates($this->normaliseAssiociates($this->friends));
+        echo '<br/><br/>';
+        $this->renderAssiociates($this->normaliseAssiociates($this->followers));
+    }
+
+    /**
+     * The function normalise the result from the API request
+     *
+     * @param ojbect $apiresults
+     * @return object
+     */
+    public function normaliseAssiociates($apiresults)
+    {
+        $assiociates = [];
+        foreach ($apiresults as $array) {
+            foreach ($array as $result) {
+                $assiociate = new stdClass();
+                if (isset($result->name)) {
+                    $assiociate->name = $result->name;
+                    $assiociate->photo = $result->photo;
+                    $assiociate->description = $result->description;
+                    $assiociate->email = $result->email;
+                    $assiociate->count = $result->follower_count;
+                } else {
+                    $assiociate->name = $result->friend_name;
+                    $assiociate->photo = $result->friend_image;
+                    $assiociate->description = $result->friend_bio;
+                    $assiociate->email = $result->email;
+                    $assiociate->count = $result->number_of_friends;
+                }
+
+                $assiociates[] = $assiociate;
+            }
+        }
+        return $assiociates;
+    }
+
+    /**
+     * Render the friend using a list-group.
+     *
+     * @param object $assiociates Object that contains Followers or Friends
+     * @return void
+     *
+     */
+    public function renderAssiociates($associates)
+    {
+        $html = '<div class="list-group">';
+
+        foreach ($associates as $associate) {
+            $html .= '<span class="list-group-item list-group-item-action d-flex gap-3 py-3" aria-current="true">';
+            $html .= '<img src="' . $associate->photo . '" alt="" width="100" height="100" class="rounded-circle flex-shrink-0">';
+            $html .= '<div class="d-flex gap-2 w-100 justify-content-between">';
+            $html .= '<div>';
+            $html .= '<h6 class="mb-0">' . $associate->name . '</h6>';
+            $html .= '<p class="mb-0 opacity-75">' . $associate->description . '</p>';
+            $html .= '<p class="mb-0 opacity-75"><a href="' . $associate->email . '">' . $associate->email . ' </a></p>';
+            $html .= '</div>';
+            $html .= '<small class="opacity-50 text-nowrap">' . $associate->count . '</small>';
+            $html .= '</div>';
+            $html .= '</span>';
+        }
+
+        $html .= '</div>';
+
+        echo $html;
     }
 }
